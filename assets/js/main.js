793dc9d1797bc17f14c68f3e61b9f43361abdc5c
Dronett <cramail.g@gmail.com>
2025-08-25 22:46:04 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
+
+function toggleMenu(){document.querySelector('header ul').classList.toggle('show');}
 document.getElementById('year').textContent=new Date().getFullYear();
+// Animation au scroll
+const faders=document.querySelectorAll('.fade-in');
+const options={threshold:0.2};
+const observer=new IntersectionObserver((entries,observer)=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){entry.target.classList.add('visible');observer.unobserve(entry.target);}
+  });
+},options);
+faders.forEach(f=>{observer.observe(f);});
